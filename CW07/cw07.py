f7e907f9b1c7aaa59341da87ee6a6875a762799a
Yamil Farah <2509266@upy.edu.mx>
2026-06-11 17:25:29 -0600

    Multiplicar por los numero de frecuencia

--- CW07/cw07.py
+++ CW07/cw07.py
@@ -6,3 +6,12 @@ rol = input("Ingrese el rol sin digito verificador: ")
 
 #Invertir el numero 
 rol_invertido = rol[::-1]
+
+#Multiplicar los digitos por la secuencia
+multiplicadores = [2, 3, 4, 5, 6, 7]
+suma = 0
+
+for i in range(len(rol_invertido)):
+    digito = int(rol_invertido[i])
+    multiplicador = multiplicadores[i % len(multiplicadores)]
+    suma += digito * multiplicador
